26ac6429eacc4cfc6994dd8640b6cd49467e7b13
Joaquin Maguire <thejmags@gmail.com>
2023-06-08 14:40:31 -0700

    moved stylesheet; trying to run  a server

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.static('static'));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(8080, () => {
+    console.log('Server running on port 8080');
+});
